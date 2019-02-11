08b98d91458f14f5ee69c8cead3bfab84425ed7a
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2019-02-11 21:47:12 +0100

    Add files via upload

---
+++ test/runtests.jl
@@ -0,0 +1,12 @@
+using Test
+using CategoricalDataAnalysis
+using MIDI
+
+cd(@__DIR__)
+#testing the functions with a piece in C major from Haydn
+tester = [MIDI.PITCH_TO_NAME[mod(n.pitch, 12)] for n in getnotes(readMIDIfile("2_2.mid"))]
+#filtering out the outside notes
+tester = filter(x -> x in(["C","B","A","D","G","E","F"]), d)
+
+@test cramer_coefficient(d,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],["C","E"])[2][6] == 0.4145144779175194
+@test LaggedBivariateProbability(d,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],"C","E")[2][6] == 0.009615384615384616
