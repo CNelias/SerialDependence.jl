e8c862771b198cbd687d6a1c655b1794c01dccf1
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2019-02-12 15:19:34 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,5 +8,5 @@ tester = [MIDI.PITCH_TO_NAME[mod(n.pitch, 12)] for n in getnotes(readMIDIfile("2
 #filtering out the outside notes
 tester = filter(x -> x in(["C","B","A","D","G","E","F"]), tester)
 
-@test cramer_coefficient(d,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],["C","E"])[2][6] == 0.4145144779175194
-@test LaggedBivariateProbability(d,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],"C","E")[2][6] == 0.009615384615384616
+@test cramer_coefficient(tester,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],["C","E"])[2][6] == 0.4145144779175194
+@test LaggedBivariateProbability(tester,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],"C","E")[2][6] == 0.009615384615384616
